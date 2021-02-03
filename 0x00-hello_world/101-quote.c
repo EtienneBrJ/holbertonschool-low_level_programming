ddf62fe9bea1b66b4c4c92dfb6eef8975f085f9e
Etienne Brun <etienne.brunj@gmail.com>
2021-02-03 15:13:31 +0000

    change value stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
-		}
+}
