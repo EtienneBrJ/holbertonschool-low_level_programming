bec73d9dd05837876c9c91d87177444a18b66b39
Etienne Brun <etienne.brunj@gmail.com>
2021-02-03 11:30:46 +0000

    Add Task 1/5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Output "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char Mystring[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystring);
+
+	return (0);
+}
