d35978f9a87d052fe248199cc79ca9d29bb1cc84
Etienne Brun <etienne.brunj@gmail.com>
2021-02-08 17:39:37 +0000

    Add Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Print every minute of the day of J.B starting from 00:00
+ * @void: Null
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
