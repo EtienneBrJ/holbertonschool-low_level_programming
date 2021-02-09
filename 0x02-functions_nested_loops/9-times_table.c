052cddbca25938ee0b1d91bec2c64ec7d24538af
Etienne Brun <etienne.brunj@gmail.com>
2021-02-09 10:43:40 +0000

    Fix Tabs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,13 +21,13 @@ void times_table(void)
 			if (k < 10 && m != 0)
 			{
 				_putchar(',');
-			        _putchar(' ');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
 			{
 				_putchar(',');
-			        _putchar(' ');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
