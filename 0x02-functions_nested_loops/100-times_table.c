04bbdd87f69cb3a2bc30b858f163c0ea356af165
Etienne Brun <etienne.brunj@gmail.com>
2021-02-09 18:30:11 +0000

    Add Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - Print times table for 0 to 15 included
+ * @n: Integer to multiply
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	k = 0;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (k < 10 || k == 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				if (k >= 10 && k <= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100 && k <= 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
