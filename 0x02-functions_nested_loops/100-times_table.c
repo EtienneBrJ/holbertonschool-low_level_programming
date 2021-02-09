f14cff176debcd6dd509868bce772a054735809c
Etienne Brun <etienne.brunj@gmail.com>
2021-02-09 18:56:58 +0000

    Fix algo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,29 +18,30 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
-				if (k < 10 || k == 0)
+				if (j > 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				if (k >= 10 && k <= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100 && k <= 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					if (k < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					else if (k >= 10 && k < 100)
+					{
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+					else if (k >= 100)
+					     {
+						     _putchar((k / 100) + '0');
+						     _putchar(((k / 10) % 10) + '0');
+						     _putchar((k % 10) + '0');
+					     }
+					else
+						_putchar(k + '0');
 				}
 			}
 			_putchar('\n');
