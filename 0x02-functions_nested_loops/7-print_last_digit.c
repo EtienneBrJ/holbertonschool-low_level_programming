5285c1f2a787b5b021ca2e2963c6c21456249bab
Etienne Brun <etienne.brunj@gmail.com>
2021-02-09 09:02:38 +0000

    Fix loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,17 @@
 /**
  * print_last_digit - Print the last digit of a number
  * @n: Number
- * Return: 0
+ * Return: a
  */
 int print_last_digit(int n)
 {
 	int a;
 
 	if (n < 0)
+	{
 		n = -n;
-		a = n % 10;
+	}
+	a = n % 10;
 	_putchar(a + '0');
 	return (a);
 }
