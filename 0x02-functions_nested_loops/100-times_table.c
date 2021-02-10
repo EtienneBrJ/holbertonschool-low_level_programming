902392fdd59cdb251d3f6fac0e028b8f4db040e6
Etienne Brun <etienne.brunj@gmail.com>
2021-02-10 10:26:45 +0000

    FixBetty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,9 +36,9 @@ void print_times_table(int n)
 					}
 					else if (k >= 100)
 					{
-						     _putchar((k / 100) + '0');
-						     _putchar(((k / 10) % 10) + '0');
-						     _putchar((k % 10) + '0');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
 					}
 				}
 				else
