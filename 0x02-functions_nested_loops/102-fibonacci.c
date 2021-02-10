c242633a10006d25f377b1d577bc931c992b058f
Etienne Brun <etienne.brunj@gmail.com>
2021-02-10 23:14:10 +0000

    Fix b = a + b;

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,16 @@
 int main(void)
 {
 	int long a, b ,c;
+	int i;
 
 	a = 1;
 	b = 2;
 	for (i = 0; i < 50; i++)
 	{
 		c = a + b;
-		printf("%d, ", c);
-		c = b;
-		b = a + b;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
 	}
 	printf("\n");
 	return (0);
