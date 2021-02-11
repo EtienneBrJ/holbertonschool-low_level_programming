ce946e08abf08afa1a70f599aa7917a6af38b56e
Etienne Brun <etienne.brunj@gmail.com>
2021-02-11 14:05:49 +0000

    Add Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i == 100)
+		{
+			printf("Buzz");
+		}
 		if (i % 5 == 0 && i % 3 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0 && !(i == 100))
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else
+		else if (i != 100)
 			printf("%d ", i);
 	}
 	printf("\n");
