9197758357ce61e4958788881cb42cabaabc8f3f
Etienne Brun <etienne.brunj@gmail.com>
2021-02-11 19:35:23 +0000

    Add Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - find and print the largest prime numbers of num
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+	int long num;
+
+	num = 612852475143;
+
+	for (count = 2; num > 1; count++)
+	{
+		while (num % count == 0)
+		{
+			num = num / count;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
