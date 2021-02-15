0d136c5240ae4b416919170a97108ee9cbca15de
Etienne Brun <etienne.brunj@gmail.com>
2021-02-15 17:52:18 +0000

    Add Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: value of the variable S
+ */
+void print_rev(char *s)
+{
+	int plus = 0;
+	int i;
+
+	while (s[plus] != '\0')
+	{
+		plus++;
+	}
+	for (i = plus; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
