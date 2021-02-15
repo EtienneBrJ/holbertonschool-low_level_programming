795dac5a842ce091f21b7cf374629e72b5f2fe0c
EtienneBrJ <etiennebrxv@gmail.com>
2021-02-15 13:42:48 +0000

    Add Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - swap the value of two int
+ *@a: Int to swap
+ *@b: Int to swap
+ */
+void swap_int(int *a, int *b)
+{
+  int temporaire = 0;
+
+  temporaire = *a;
+  *a = *b;
+  *b = temporaire;
+}
