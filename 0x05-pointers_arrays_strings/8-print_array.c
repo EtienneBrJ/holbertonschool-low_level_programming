c12c1cc48d1c177a82b1169ced66f478ae961827
Etienne Brun <etienne.brunj@gmail.com>
2021-02-16 11:24:42 +0000

    Fix loop error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,23 +7,17 @@
  */
 void print_array(int *a, int n)
 {
-	int l;
+	int i;
 
-	n = 0;
-
-	while (a[n] != '\0')
+	for (i = 0; i < n; i++)
 	{
-		n++;
-	}
-	for (l = 0; l < n - 1; l++)
-	{
-		if (l != 0)
+		if (i != 0)
 		{
-			printf(", %d", a[l]);
+			printf(", %d", a[i]);
 		}
 		else
 		{
-			printf("%d", a[l]);
+			printf("%d", a[i]);
 		}
 	}
 	printf("\n");
