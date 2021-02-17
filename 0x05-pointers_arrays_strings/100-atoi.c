61f5bd083ee6430fa6968e1b3c48319710764f62
Etienne Brun <etienne.brunj@gmail.com>
2021-02-17 20:15:08 +0000

    Add Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,31 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  *
  *
  */
 int _atoi(char *s)
 {
-	
+	int i, sign, result;
+
+	i = result = 0;
+	sign = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (s[i] <= '0' && s[i] >= '9')
+			break;
+		i++;
+	}
+	result *= sign;
+	return (result);
 }
