4f79b9f5d1b85c2d14a1343dbbecad17624baa16
Etienne Brun <etienne.brunj@gmail.com>
2021-02-18 13:35:51 +0000

    Add Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string
+ * @dest: string that will receive the copy
+ * @src: string to be copied
+ * @n: bytes of string src
+ * Return: @dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
