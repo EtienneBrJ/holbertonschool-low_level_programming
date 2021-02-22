8c4a5bc7299e1381a0c1e24a7250b4a3154f7c43
Etienne Brun <etienne.brunj@gmail.com>
2021-02-22 12:26:36 +0000

    Add Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: where is the first occurence
+ * Return: a pointer to the beggining of the located substring.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	char *p = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			p = &haystack[i];
+			break;
+		}
+	}
+	return (p);
+}
