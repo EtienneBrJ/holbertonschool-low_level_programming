09767a90a92f2d39945fbdb461e44e9ba27f4d29
Etienne Brun <etienne.brunj@gmail.com>
2021-02-22 14:08:07 +0000

    Add Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,20 +9,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char *r = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				r = &s[i];
+				return (&s[i]);
 		}
 	}
-	if (r == 0)
-	{
-		return (NULL);
-	}
-	else
-		return (r);
+	return (NULL);
 }
