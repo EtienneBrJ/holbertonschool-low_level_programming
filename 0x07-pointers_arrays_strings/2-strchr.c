a7689e3138910cccd99a35734b23167993c07aee
Etienne Brun <etienne.brunj@gmail.com>
2021-02-22 10:16:01 +0000

    Add Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr - locate a char in a string
+ * @s: string to check
+ * @c: char to locate
+ * Return: first occurence of the char c in the string s,
+ * or NULL if char not found
+ */
+char *_strchr(char *s, char c)
+{
+	char *s2 = 0;
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s2 = &s[i];
+		}
+	}
+	return (s2 - 1);
+}
