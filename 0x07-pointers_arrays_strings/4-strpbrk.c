340a7877f968e3a8ded983389b4e29bf2b4ac5c8
Etienne Brun <etienne.brunj@gmail.com>
2021-02-22 11:55:18 +0000

    Add Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: string to find occurence
+ * @accept: string where is occurence to find is s
+ * Return: pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *r = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				r = &s[i];
+		}
+	}
+	return (r);
+}
