890f0e54e420f1bf90c2ba1d6c3ce96209512939
Etienne Brun <etienne.brunj@gmail.com>
2021-02-23 12:25:23 +0000

    Fix algo

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int j;
+	int i = 0;
 
-	char *p = 0;
-
-	for (i = 0; haystack[i] >= '\0'; i++)
+	if (needle == '\0')
 	{
-		if (haystack[i] == needle[0])
+		return (&haystack[i]);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] == haystack[i + j]; j++)
 		{
-			p = &haystack[i];
-			break;
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
 		}
-		return (p);
 	}
 	return (NULL);
 }
