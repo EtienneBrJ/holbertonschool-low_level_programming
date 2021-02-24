38effb8e2257710c569b5a3804f1081c80689181
Etienne Brun <etienne.brunj@gmail.com>
2021-02-24 15:56:34 +0000

    Change _guess_sqrt function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,34 +9,22 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_guess_sqrt(n, 1, n));
+	return (_guess_sqrt(n, 0));
 }
 
 /**
  * _guess_sqrt - returns the square root of a number
  * @n: number to check
- * @min: min delimitation for check
- * @max: max delimitation for check
+ * @guess: number testing for square root of n
  * Return: square root of n
  */
 
-int _guess_sqrt(int n, int min, int max)
+int _guess_sqrt(int n, int guess)
 {
-	int guess = (min + max) / 2;
-	int guess_squared = guess * guess;
-
-	if (max < min)
-	{
-		return (-1);
-	}
-	if (guess_squared == n)
-	{
+	if (guess * guess == n)
 		return (guess);
-	}
-	else if (guess_squared < n)
-	{
-		return (_guess_sqrt(n, guess + 1, max));
-	}
+	else if (guess * guess > n)
+		return (-1);
 	else
-		return (_guess_sqrt(n, min, guess - 1));
+		return (_guess_sqrt(n, guess + 1));
 }
