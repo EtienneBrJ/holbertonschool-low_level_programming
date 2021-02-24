0c1bf5fe6ac74e20d69291ff17c70a274682e4d8
Etienne Brun <etienne.brunj@gmail.com>
2021-02-24 20:03:42 +0000

    Fix strlen

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -36,13 +36,7 @@ int check(char *s, int i)
 
 int _strlen(char *s)
 {
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
 }
