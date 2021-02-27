591e186ec4c56531e4665a55def84c8f10aa435e
Etienne Brun <etienne.brunj@gmail.com>
2021-02-27 19:51:07 +0000

    Add Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <ctype.h>
+int _atoi(char *s);
+/**
+ * _atoi - convert string to int
+ * @s: string to convert
+ * Return: Int
+ */
+int _atoi(char *s)
+{
+	int i, sign, result;
+	int stop_conv;
+
+	i = 0;
+	result = 0;
+	stop_conv = 0;
+	sign = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			stop_conv = 1;
+		}
+		else if (stop_conv == 1)
+			break;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+
+/**
+ * main - adds numbers
+ * @argc: numbers of paramters
+ * @argv: array of parameters
+ * Return: 0, or 1 if Error
+ **/
+
+int main(int argc, char **argv)
+{
+	int result = 0;
+	int i = 0;
+
+	while (i < argc)
+	{
+		if (i > 0)
+		{
+			if (!_atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += _atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
