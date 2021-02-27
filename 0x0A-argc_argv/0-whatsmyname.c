91130931ac76d1c5a2e00aed4df03f37eefa8618
Etienne Brun <etienne.brunj@gmail.com>
2021-02-27 18:03:07 +0000

    Add Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints the name program
+* @argc: contains the numbers of arguments
+* @argv: array stocking parameters
+* Return: 0
+**/
+
+int main(int argc, char **argv)
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
