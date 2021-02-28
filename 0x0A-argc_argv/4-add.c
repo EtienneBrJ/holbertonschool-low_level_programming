5ebacfee18e67285cd685d7a963f54bda8a6db4d
Etienne Brun <etienne.brunj@gmail.com>
2021-02-28 15:52:34 +0000

    Error condition for negative numbers?

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -47,18 +47,19 @@ int main(int argc, char **argv)
 	int result = 0;
 	int i = 1;
 
-	if (argc < 1)
+	if (argc == 0)
 	{
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!_atoi(argv[i]))
+		if (!_atoi(argv[i]) || _atoi(argv[i]) < 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		result += _atoi(argv[i]);
+		else if (_atoi(argv[i]) > 0)
+			result += _atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
