23f77bba3db86b9c5017d066bb2b95cf7dd45449
Etienne Brun <etienne.brunj@gmail.com>
2021-03-04 14:24:02 +0000

    Add Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			i++;
 			j++;
 		}
-		s[lens1 + lens2 + 1] = '\0';
+		s[i] = '\0';
 	}
 	else
 	{
@@ -49,7 +49,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			i++;
 			j++;
 		}
-		s[lens1 + n + 1] = '\0';
+		s[i] = '\0';
 	}
 	return (s);
 }
