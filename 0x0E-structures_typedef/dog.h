a1b24a5ac648523b34e88b09c7bcfe50c4c8ebef
Etienne Brun <etienne.brunj@gmail.com>
2021-03-08 14:19:36 +0000

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - dog information
  * @name: name
@@ -16,6 +15,5 @@ struct dog
 	float age;
 };
 
-#endif
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
