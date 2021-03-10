2d88506f301a61681fefc5514e0c915256a39420
Etienne Brun <etienne.brunj@gmail.com>
2021-03-10 21:49:41 +0000

    Task 3?

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,10 @@
 #include "3-calc.h"
-
+/**
+ * main - performs simple operations
+ * @ac: argc
+ * @av: argv
+ * Return: 0
+ **/
 int main(int ac, char **av)
 {
 	int num1, num2, res;
@@ -8,14 +13,14 @@ int main(int ac, char **av)
 	if (ac != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	num1 = atoi(av[1]);
 	num2 = atoi(av[3]);
 	if (get_op_func(av[2]) == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	function = get_op_func(av[2]);
 
