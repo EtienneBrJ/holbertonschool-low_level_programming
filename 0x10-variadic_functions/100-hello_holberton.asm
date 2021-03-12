c4ee5bdbebb6c01795110b97a3ce82fda425ca00
Etienne Brun <etienne.brunj@gmail.com>
2021-03-12 13:52:48 +0000

    .

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,8 @@
+	.global main             /* Make main a global function */
+main:	                       /* Start function main */
+	mov $msg, %rdi           /* RDI gets pointer to message */
+	call puts                /* puts(msg) */
+	mov $0, %rax             /* RAX gets zero */
+	ret                      /* return(0) */
+msg:	                        /* Declare a label for the string */
+	   .asciz "Hello World"     /* Define the string */ 
