aac6356d8ec55d260ea8f7ef4e3599145b47b5d1
Etienne Brun <etienne.brunj@gmail.com>
2021-03-18 15:52:42 +0000

    add task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,14 @@
 #include "lists.h"
-
+/**
+ * print_list - print all the element of the list_t list
+ * @h : pointer to the list
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	size_t nb = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
