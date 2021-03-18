355d7bc3f8768b1926341d0c94e74aee6a6b166e
Etienne Brun <etienne.brunj@gmail.com>
2021-03-18 22:20:01 +0000

    add task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_list - frees a list
- * head - pointer to the first node of the list
+ * @head: pointer to the first node of the list
  */
 void free_list(list_t *head)
 {
