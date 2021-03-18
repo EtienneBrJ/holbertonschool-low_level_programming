1316a5876e60caeb36e66a6e580c8a7cc6fc162d
Etienne Brun <etienne.brunj@gmail.com>
2021-03-18 15:48:37 +0000

    add task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t nb = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nb++;
+	}
+	return (nb);
+}
