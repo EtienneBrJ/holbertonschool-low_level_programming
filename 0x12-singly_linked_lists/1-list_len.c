5f14dce1f9f86456a819c2458359a1e5d64a6f27
Etienne Brun <etienne.brunj@gmail.com>
2021-03-18 15:55:49 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_list - print all the element of the list_t list
+ * @h : pointer to the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nb = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nb++;
+	}
+	return (nb);
+}
