0d0e977660b58cb49048bf8f1c12999cfe9227a3
Etienne Brun <etienne.brunj@gmail.com>
2021-03-23 15:12:34 +0000

    not wrkg

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list
+ * @head: pointer to the list
+ * Return: the nb of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nb = 0;
+
+	while (head)
+	{
+		nb++;
+		printf("[%p] %d\n", (void*)head->next, head->n);
+		if (head->next)
+			head = head->next;
+		else
+			exit(98);
+	}
+	return (nb);
+}
