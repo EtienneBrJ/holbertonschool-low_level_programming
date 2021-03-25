1a878a954a326337cede02eaf6d1e583cc6c4580
Etienne Brun <etienne.brunj@gmail.com>
2021-03-25 16:35:29 +0000

    fix task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int))
+	if (index > 63)
 		return (-1);
 	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,6 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	int ch_val;
 
-	ch_val = n & ~(1 << index);
+	ch_val |= 1 << index;
 	return (ch_val);
 }
