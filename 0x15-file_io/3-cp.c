d3fbf10a446502595537197a1929a3145f8f3209
Etienne Brun <etienne.brunj@gmail.com>
2021-03-31 15:05:20 +0000

    fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,28 +5,60 @@
  * @av: double pointer of arguments
  * Return: int
  */
-int main(int ac, int **av)
+int main(int ac, char **av)
 {
 	char buf[1024];
-	int fd, fdw, fdr;
+	int fd, fdw, r, w, c;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	fdw = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
 	if (fdw == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	fdr = write(
+	r = read(fd, buf, 1024);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		exit(98);
+	}
+	while (r > 0)
+	{
+		w = write(fdw, buf, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		r = read(fd, buf, 1024);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+			exit(98);
+		}
+	}
+	c1 = close(fd);
+	c2 = close(fdw);
+	if (c1 == -1 || c2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdw);
+		exit(100);
+	}
+	return (0);
 }
