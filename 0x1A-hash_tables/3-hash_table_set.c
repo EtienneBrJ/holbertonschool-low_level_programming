c9e9f0de38591cc213faa201739d7d7129090c8e
EtienneBrJ <etiennebrxv@gmail.com>
2021-05-30 16:18:02 +0200

    add 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *value_copy;
 	unsigned long int i, index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || !value)
