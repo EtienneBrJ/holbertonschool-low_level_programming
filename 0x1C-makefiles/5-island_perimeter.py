3a44d63d71c31784684f0231d6051e1b2919b30e
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-07 00:28:53 +0200

    Add island perimeter/white paper

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module island perimeter"""
 
+
 def island_perimeter(grid):
     """ Returns the perimeter of the island
         described in grid
@@ -9,16 +10,16 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col]:
-                if row - 1 >= 0: # check for upper cell
+                if row - 1 >= 0:  # check for upper cell
                     if grid[row - 1][col] == 0:
                         perimeter += 1
-                if row + 1 >= 0: # check for lower cell
+                if row + 1 >= 0:  # check for lower cell
                     if grid[row + 1][col] == 0:
                         perimeter += 1
-                if col - 1 >= 0: # check for right cell
+                if col - 1 >= 0:  # check for right cell
                     if grid[row][col - 1] == 0:
                         perimeter += 1
-                if col + 1 >= 0: # check for left cell
+                if col + 1 >= 0:  # check for left cell
                     if grid[row][col + 1] == 0:
                         perimeter += 1
 
