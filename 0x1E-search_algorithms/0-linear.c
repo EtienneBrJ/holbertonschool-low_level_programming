fd5f4c751ec016416a9d10151676b8ecdb07dd67
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-30 17:50:12 +0300

    Initial commit: Algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in a sorted array of integers
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where value is located or -1 if not found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	int i_value = 0;
+	int flag = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value && flag == 0)
+		{
+			i_value = i;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1)
+		return (i_value);
+	else
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+  * print_array - print array with limits in l and r
+  * @array: pointer to the first element of the array to search in
+  * @l: low
+  * @h: high
+  */
+void print_array(int *array, size_t l, size_t h)
+{
+	printf("Searching in array: ");
+	while (l < h)
+	{
+		printf("%d, ", array[l]);
+		l++;
+	}
+	printf("%d\n", array[l]);
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where value is located or -1 if not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l = 0, h = size - 1, mid = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (l <= h)
+	{
+		print_array(array, l, h);
+		mid = (l + h) / 2;
+		printf("%ld\n", mid);
+		if (array[mid] < value)
+			l = mid + 1;
+		else if (array[mid] > value)
+			h = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
